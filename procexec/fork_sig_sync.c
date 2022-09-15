020c11d48d67414485ae325d1229dbe201fbc12f
Cu63 <scube.str@gmail.com>
2022-09-15 03:58:39 -0400

    init

---
+++ procexec/fork_sig_sync.c
@@ -0,0 +1,60 @@
+#include <signal.h>
+#include "curr_time.h"
+#include "tlpi_hdr.h"
+
+
+#define SYNC_SIG SIGUSR1 //сигнал синхронизации
+
+/* Обработчик сигнала, ничего не делает, просто завершается  */
+static void handler(int sig) {
+}
+
+
+int main(int argc, char *arfv[]) {
+    pid_t childPid;
+    sigset_t blockMask, origMask, emptyMask;
+    struct sigaction sa;
+
+    setbuf(stdout, NULL);
+
+    sigemptyset(&blockMask);
+    sigaddset(&blockMask, SYNC_SIG); //Блокируем сигнал
+    if (sigprocmask(SIG_BLOCK, &blockMask, &origMask) == -1)
+        errExit("sigprocmask");
+
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    sa.sa_handler = handler;
+    if (sigaction(SYNC_SIG, &sa, NULL) == -1)
+        errExit("sigaction");
+
+    switch (childPid = fork()) {
+        case -1:
+            errExit("fork");
+
+        case 0: //Потомок
+            printf("[%s %ld] Child started - doing some work\n",
+                    currTime("%T"), (long) getpid());
+            sleep(2);
+
+            printf("[%s %ld] Child about to signal parent\n",
+                    currTime("%T"), (long) getpid());
+            if (kill(getpid(), SYNC_SIG) == -1)
+                errExit("kill");
+
+            _exit(EXIT_SUCCESS);
+
+        default: //Родитель
+            printf("[%s %ld] Parent about to wati for signal\n",
+                  currTime("%T"), (long) getpid());
+            sigemptyset(&emptyMask);
+            if (sigsuspend(&emptyMask) == -1 && errno != EINTR)
+                errExit("sigsuspend");
+            printf("[%s %ld] Parent got signal\n", currTime("%T"), (long) getpid());
+
+            if (sigproc(SIG_SETMASK, &origMask, NULL) == -1)
+                errExit("sigproc");
+
+            exit(EXIT_SUCCESS);
+    }
+}
